e4edd3951a8dd177494eee98978a4ae3428d2f53
Amanat1599 <154678817+Amanat1599@users.noreply.github.com>
2024-08-16 16:29:17 +0500

    First commit

---
+++ task.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import pyodbc
+
+conn_string="Driver=ODBC Driver 18 for SQL Server;Server=tcp:serveru123.database.windows.net,1433;Database=DB123;Uid=server123-UID;Pwd=P@ssword;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+connection=pyodbc.connect(conn_string)
+df1=pd.read_sql_query("""Select * from digi""",connection)
+print(df1)
+print("Hellow world")
